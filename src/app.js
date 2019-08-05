da528a8f97d213174a255c8d362b9764a1f6c379
dionlopes <dion.mello@hotmail.com>
2019-08-05 18:45:09 -0300

    feat: Criando Routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import routes from './routes';
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    this.routes();
+  }
+
+  routes() {
+    this.server.use(routes);
   }
 }
 
