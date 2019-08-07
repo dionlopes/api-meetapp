5a18ca21bf424fc81fe45fdf50c3d307c946e42f
Dion <dion.mello@hotmail.com>
2019-08-07 16:16:27 -0300

    chore: Configurando cors na aplicação

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import 'express-async-errors';
 
 import path from 'path';
+import cors from 'cors';
 
 import Youch from 'youch';
 
@@ -29,6 +30,7 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(cors());
     this.server.use(express.urlencoded({ extended: false }));
     this.server.use(
       '/files',
